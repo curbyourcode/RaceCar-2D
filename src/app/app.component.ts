92947504b82b3ec2a0162e579189a5f8371d77ec
curbyourcode <majlessirouz@gmail.com>
2019-01-08 16:08:11 -0800

    add loading game and creating assetts for game in app component use eventemitter to get updatres from service and add method to dowload game assetts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+/**
+    Building a 2D racing Game using Angular
+    @auther Rouz Majlessi
+*/
+
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+import { AppService } from './services/app.service';
+import { GameService } from './services/game.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'race-car';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('canvas') public canvas: ElementRef;
+  subscription: any;
+  showLoader = true;
+
+  constructor(private appService: AppService, private gameService: GameService) {}
+
+  public ngAfterViewInit() {
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.appService.createPlayground(canvasEl);
+    this.subscription = this.appService.getImageLoadEmitter()
+      .subscribe((item) => {
+        this.showLoader = false;
+        this.gameService.startGameLoop();
+      });
+  }
 }
