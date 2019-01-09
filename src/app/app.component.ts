c35362aa7e65b9597e12498364a858763dc51662
curbyourcode <majlessirouz@gmail.com>
2019-01-08 16:40:02 -0800

    add hostlistner to handle user input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,51 @@
     @auther Rouz Majlessi
 */
 
-import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  AfterViewInit,
+  ViewChild,
+  HostListener
+} from "@angular/core";
 import { AppService } from './services/app.service';
 import { GameService } from './services/game.service';
 
 @Component({
-selector: 'app-root',
-templateUrl: './app.component.html',
-styleUrls: ['./app.component.css'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit {
-@ViewChild('canvas') public canvas: ElementRef;
-subscription: any;
-showLoader = true;
+  @ViewChild('canvas') public canvas: ElementRef;
+  subscription: any;
+  showLoader = true;
 
-constructor( private appService: AppService, private gameService: GameService ) {}
+  constructor(
+    private appService: AppService,
+    private gameService: GameService
+  ) {}
 
-public ngAfterViewInit() {
-const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
-this.appService.createPlayGround(canvasEl);
-this.subscription = this.appService.getImageLoadEmitter()
-.subscribe((item) => {
-this.showLoader = false;
-this.gameService.startGameLoop();
-});
-}
+  public ngAfterViewInit() {
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.appService.createPlayGround(canvasEl);
+    this.subscription = this.appService
+      .getImageLoadEmitter()
+      .subscribe(item => {
+        this.showLoader = false;
+        this.gameService.startGameLoop();
+      });
+  }
+
+  @HostListener('document:keydown', ['$event']) onKeydownHandler(
+    event: KeyboardEvent
+  ) {
+    this.appService.movePlayer(event, 'keydown');
+  }
+
+  @HostListener('document:keyup', ['$event']) onKeyupHandler(
+    event: KeyboardEvent
+  ) {
+    this.appService.movePlayer(event, 'keyup');
+  }
 }
