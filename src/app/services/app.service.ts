252e6c8d09bdeafa8b3aaea7a1b61bb7aa54c10e
curbyourcode <majlessirouz@gmail.com>
2019-01-08 16:41:26 -0800

    add user handling in app services

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -8,17 +8,46 @@ import { GameService } from './game.service';
 
 @Injectable()
 export class AppService {
+  isImageLoaded: EventEmitter<number> = new EventEmitter();
+  constructor(private gameService: GameService) {}
 
-isImageLoaded: EventEmitter<number> = new EventEmitter();
-constructor(private gameService: GameService) { }
+  createPlayGround(canvasElement): void {
+    this.gameService.loadAssets(canvasElement).then(image => {
+      this.isImageLoaded.emit();
+    });
+  }
 
-createPlayGround(canvasElement): void {
-this.gameService.loadAssets(canvasElement).then( (image) => {
-this.isImageLoaded.emit();
-});
-}
+  getImageLoadEmitter() {
+    return this.isImageLoaded;
+  }
 
-getImageLoadEmitter() {
-return this.isImageLoaded;
-}
+  movePlayer(event: KeyboardEvent, type: string): void {
+    if (type === 'keydown') {
+      if (event.keyCode === 37) {
+        this.gameService.moveLeft = true;
+        this.gameService.moveUP = false;
+        this.gameService.moveDown = false;
+      } else if (event.keyCode === 39) {
+        this.gameService.moveRight = true;
+        this.gameService.moveLeft = false;
+        this.gameService.moveUP = false;
+        this.gameService.moveDown = false;
+      } else if (event.keyCode === 38) {
+        this.gameService.moveUP = true;
+        this.gameService.moveLeft = false;
+        this.gameService.moveRight = false;
+        this.gameService.moveDown = false;
+      } else if (event.keyCode === 40) {
+        this.gameService.moveDown = true;
+        this.gameService.moveLeft = false;
+        this.gameService.moveRight = false;
+        this.gameService.moveUP = false;
+      }
+    } else if (type === 'keyup') {
+      this.gameService.moveDown = false;
+      this.gameService.moveLeft = false;
+      this.gameService.moveRight = false;
+      this.gameService.moveUP = false;
+    }
+  }
 }
